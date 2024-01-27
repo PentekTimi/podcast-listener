728ad9038853e284942c5b2ef894200450ba0914
PentekTimi <pentektimea98@gmail.com>
2024-01-27 21:30:09 +0000

    Initialize project

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <!-- navbar -->
+  <div>
+    <div>
+      <h2>PODCASTIN</h2>
+    </div>
+    <div>
+      <ul>
+        <li><a>Log In</a></li>
+        <li><a>Sign Up</a></li>
+      </ul>
+    </div>
+  </div>
+
+  <!-- introduction heading -->
+  <div>
+    <h1>DISCOVER</h1>
+    <img src="" />
+    <h1>NEW</h1>
+    <img src="" />
+    <h1>PODCASTS</h1>
+    <img src="" />
+    <h1>DAILY</h1>
+  </div>
+
+  <!-- main content -->
+  <div>
+    <!-- introduction text and button -->
+    <div>
+      <p>
+        Award-winning podcasts that hook you in, make you think, or just make you laugh. Sign up and
+        listen now.
+      </p>
+      <button>Get Started</button>
+      <span>arrow</span>
+    </div>
+
+    <!-- supported by -->
+    <div>
+      <p>Supported by</p>
+      <div>
+        <img src="" />
+        <img src="" />
+        <img src="" />
+        <img src="" />
+      </div>
+    </div>
+
+    <!-- top podcasts of the week -->
+    <div>
+      <div>
+        <h4>Top Podcasts of the week</h4>
+        <div>
+          <!-- individual podcast -->
+          <div>
+            <p>01.</p>
+            <img src="" />
+            <p>Podcast Title</p>
+            <p>#category</p>
+          </div>
+        </div>
+      </div>
+      <div>scroller</div>
+    </div>
+  </div>
+</template>
